9abf8eeecaeb51b7ea2fcf496eecfbd481fa9e31
5C24 <you_5c24@163.com>
2022-07-15 14:18:27 +0800

    feat: easy-concat

--- type-challenges/first-of-array/template.ts
+++ type-challenges/first-of-array/template.ts
@@ -5,7 +5,7 @@
 // type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
 // 3. extends union 判断的规则
 // type First<T extends any[]> = T[0] extends T[number] ? T[0] : never;
-// 4. inter 的使用
+// 4. infer 的使用
 type First<T extends any[]> = T extends [infer First, ...infer Rest]
   ? First
   : never;
